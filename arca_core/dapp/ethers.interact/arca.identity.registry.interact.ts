992445a13962ec0bf3349126bb0bf3439f378e7b
Prince Igwe <igwep297@gmail.com>
2026-01-05 23:11:23 +0100

    patient registration with guardians for social recovery

--- arca_core/dapp/ethers.interact/arca.identity.registry.interact.ts
+++ arca_core/dapp/ethers.interact/arca.identity.registry.interact.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const providerUrl = process.env.PROVIDER_URL || "http://localhost:8545";
 const provider = new ethers.JsonRpcProvider(providerUrl);
 
-const arcaIdentityRegistryAddress = "0x4A679253410272dd5232B3Ff7cF5dbB88f295319";
+const arcaIdentityRegistryAddress = "0x67d269191c92Caf3cD7723F116c85e6E9bf55933";
 const hardhatPrivateKey1 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const dummyEOAddressPrivateKey1 = process.env.WALLET_PRIVATE_KEY || hardhatPrivateKey1;
@@ -32,13 +32,15 @@ async function registerPatient(
     const dateHex = ethers.toBeHex(BigInt(dateString));
     const dateBytes32 = ethers.zeroPadValue(dateHex, 32);
 
+    // listening for event
+    arcaIdentityRegistryContractConnect1.once(
+      "PatientRegisteredEvent",
+      (message, patientIdentity) => {
+        console.log("EVENT:", message, patientIdentity);
+      }
+    );
+
     if (!linkedAddresses && !guardiansRequired && !guardians) {
-      arcaIdentityRegistryContractConnect1.once(
-        "PatientRegisteredEvent",
-        (message, patientIdentity) => {
-          console.log("EVENT:", message, patientIdentity);
-        }
-      );
       const tx = await arcaIdentityRegistryContractConnect1.registerPatient(
         dateBytes32
       );
@@ -46,21 +48,34 @@ async function registerPatient(
     }
 
     if (linkedAddresses && !guardiansRequired && !guardians) {
-      // listen for event
-      arcaIdentityRegistryContractConnect1.once(
-        "PatientRegisteredEvent",
-        (message, patientIdentity) => {
-          console.log("EVENT:", message, patientIdentity);
-        }
-      );
+      // converting strings to Ethereum Owned Addresses
       linkedAddresses.forEach(async (linkedAddress) => {
         getAddress(linkedAddress);
       });
-      
+
       const tx = await arcaIdentityRegistryContractConnect1.registerPatientWithLinkedAddresses(
           linkedAddresses,
           dateBytes32
         );
+      await tx.wait();
+    }
+
+    if (linkedAddresses && guardiansRequired && guardians) {
+      // converting strings to Ethereum Owned Addresses
+      linkedAddresses.forEach(async (linkedAddress) => {
+        getAddress(linkedAddress);
+      });
+
+      guardians.forEach(async (guardian) => {
+        getAddress(guardian);
+      });
+
+      const tx = await arcaIdentityRegistryContractConnect1.registerPatientWithLinkedAddressAndGuardians(
+        linkedAddresses,
+        guardiansRequired,
+        guardians,
+        dateBytes32
+      )
       await tx.wait();
     }
   } catch (error) {
@@ -82,6 +97,12 @@ const linkedAddresses = [
   "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
 ];
 
+const guardians = [
+  "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+  "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65"
+]
+
 // getIdentityCount()
 // registerPatient()
-registerPatient(linkedAddresses);
+// registerPatient(linkedAddresses);
+registerPatient(linkedAddresses, 2, guardians)
